0371b07c1c29a3d351f364e1672d372794a0490a
root <root@plmtipc4.intranet.epfl.ch>
2025-05-12 15:03:52 +0200

    PR commit rajaratnam

--- app-piraven.py
+++ app-piraven.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, coucou, DevOps!"
+    return "Hello, coucou, hello, DevOps!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
